d7487d285dc214b0cac9e90357ed67f7168b9f31
Emily <psyeh2@nottingham.ac.uk>
2023-10-26 21:59:28 +0100

    Link accept button to Home Page

--- app/src/main/java/com/example/feelgoodinc/fragments/TutorialFragment4.java
+++ app/src/main/java/com/example/feelgoodinc/fragments/TutorialFragment4.java
@@ -1,12 +1,16 @@
 package com.example.feelgoodinc.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.fragment.app.Fragment;
+
+import com.example.feelgoodinc.HomePage;
 import com.example.feelgoodinc.R;
 
 public class TutorialFragment4 extends Fragment {
@@ -17,6 +21,20 @@ public class TutorialFragment4 extends Fragment {
         View v = inflater.inflate(R.layout.fragment_tutorial4, container, false);
         TextView tv = (v.findViewById(R.id.eulaText));
         tv.setMovementMethod(new ScrollingMovementMethod());
+
+        Button acceptButton = v.findViewById(R.id.acceptEulaButton);
+        acceptButton.setOnClickListener(v1 -> {
+            if (getActivity() != null) {
+                startActivity(new Intent(getActivity(), HomePage.class));
+                getActivity().finish();
+            }
+        });
+
         return v;
+
+
     }
+
+
+
 }
