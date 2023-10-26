d57ed5e65e82ecc5a453077da1c1fd3a44001e9b
Emily <psyeh2@nottingham.ac.uk>
2023-10-26 14:48:08 +0100

    Made eula string a resource

--- app/src/main/java/com/example/feelgoodinc/fragments/TutorialFragment4.java
+++ app/src/main/java/com/example/feelgoodinc/fragments/TutorialFragment4.java
@@ -17,29 +17,6 @@ public class TutorialFragment4 extends Fragment {
         View v = inflater.inflate(R.layout.fragment_tutorial4, container, false);
         TextView tv = (v.findViewById(R.id.eulaText));
         tv.setMovementMethod(new ScrollingMovementMethod());
-
-        String eula = "Feel Good Inc. should not be viewed as a substitute for therapy or other psychological or " +
-                "psychiatric treatment, nor should they be considered a substitute for a diagnosis or" +
-                "medical advice.\n\n" +
-                "Notwithstanding the scientific research on journaling, Feel Good Inc. makes no representation" +
-                "or warranty that the User’s mental health will improve. Reliance on the Apps and the" +
-                "information they contain is strictly at the User’s risk. If the App licensee has been" +
-                "diagnosed with a psychiatric condition they may wish to speak to their medical" +
-                "practitioner before use.\n\n" +
-                "Feel Good Inc. makes no representation or warranty that the Apps will prevent, cure or treat" +
-                "diseases, disorders or conditions, including those related to cognition and the brain, but" +
-                "not limited to such.\n\n" +
-                "Feel Good Inc. has designed the Apps for general information purposes only. We make no" +
-                "representation or warranty regarding accuracy, completeness, suitability and safety. The" +
-                "User assumes full responsibility and Feel Good Inc. is not liable for any consequences resulting" +
-                "from reliance.\n\n" +
-                "The Apps should not be viewed as a substitute for addiction or rehabilitation programmes." +
-                "Some of the Apps are designed to increase productivity and help manage habits. Although" +
-                "the User may find them helpful in overcoming addictions, Feel Good Inc. makes no" +
-                "representation or warranty that they are fit for this purpose. The App licensee should not" +
-                "delay in obtaining such intervention as a result of usage.\n\n";
-
-        tv.setText(eula);
         return v;
     }
 }
