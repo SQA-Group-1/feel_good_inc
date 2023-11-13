7ec928334b0f598b352ed3097b5ae64c8ffd5fa0
atris <atrisyeung@gmail.com>
2023-11-13 16:58:18 +0000

    Check if journal text is empty #46

--- app/src/main/java/com/example/feelgoodinc/fragments/MoodFragment.java
+++ app/src/main/java/com/example/feelgoodinc/fragments/MoodFragment.java
@@ -15,6 +15,8 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.feelgoodinc.R;
 import com.example.feelgoodinc.database.JournalDatabaseHelper;
 import com.example.feelgoodinc.database.MoodDatabaseHelper;
@@ -25,11 +27,20 @@ import com.google.android.material.textfield.TextInputEditText;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 /**
- * A simple {@link Fragment} subclass.
+ * A Fragment that allows the user to record their mood and associated journal entry for a given date.
+ * The user can choose a mood from a set of predefined options and enter a journal text.
+ * The selected mood and journal entry can be confirmed and saved to a database.
+ *
+ * This fragment includes UI elements such as a RadioGroup for mood selection, a button to confirm
+ * the mood and journal entry.
+ *
+ * The class utilizes a MoodDatabaseHelper and JournalDatabaseHelper for managing mood and journal data storage.
+ * MoodFragment also provides a method to convert the selected mood text to the corresponding MoodType.
+ *
  */
 public class MoodFragment extends Fragment {
-
     JournalDatabaseHelper journalDatabaseHelper;
     MoodDatabaseHelper moodDatabaseHelper;
     Journal journalObj;
@@ -42,7 +53,7 @@ public class MoodFragment extends Fragment {
         Button confirmButton = view.findViewById(R.id.moodComfirmButton);
         TextView dateText = view.findViewById(R.id.date);
         TextInputEditText journalEntry = view.findViewById(R.id.journalEntry);
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat currentDate = new SimpleDateFormat("dd/MM/yyyy");
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat currentDate = new SimpleDateFormat("dd.MM.yyyy");
         Date todayDate = new Date();
         String dateStr = currentDate.format(todayDate);
         dateText.setText(dateStr);
@@ -50,21 +61,27 @@ public class MoodFragment extends Fragment {
         moodDatabaseHelper = new MoodDatabaseHelper();
         journalDatabaseHelper = new JournalDatabaseHelper();
 
+        RadioButton selectedRadioButton = view.findViewById(moodsGroup.getCheckedRadioButtonId());
+        selectedRadioButton.setTextColor(Color.BLUE);
 
         confirmButton.setOnClickListener(view1 -> {
             String selectedMood = "";
             int selectedId = moodsGroup.getCheckedRadioButtonId();
-
             if(selectedId != -1) {
                 RadioButton moodButton = (RadioButton) view.findViewById(selectedId);
                 selectedMood = moodButton.getText().toString();
                 moodObj = new Mood(convertTextToMoodType(selectedMood), todayDate);
-                moodDatabaseHelper.addNewMood(moodObj,getActivity());
             }
 
             String journalText = String.valueOf(journalEntry.getText());
-            journalObj = new Journal("THIS IS TESTING ONLY", todayDate, todayDate, journalText);
-            journalDatabaseHelper.addNewJournal(journalObj,getActivity());
+            if(journalText.length() == 0) {
+                Toast.makeText(getActivity(), "Don't forget to write something for your feeling!", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                journalObj = new Journal("THIS IS TESTING ONLY", todayDate, todayDate, journalText);
+                journalDatabaseHelper.addNewJournal(journalObj, getActivity());
+                moodDatabaseHelper.addNewMood(moodObj,getActivity());
+            }
         });
 
 
@@ -104,8 +121,6 @@ public class MoodFragment extends Fragment {
                 return null;
         }
     }
-
-
 
 
 }
