7d17263fd8919507e03be0ed297e1ae0626f7194
psyhs7 <psyhs7@nottingham.ac.uk>
2023-11-14 03:14:19 +0000

    add espresso dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.firebase:firebase-firestore:24.9.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.test:runner:1.4.0")
+    androidTestImplementation ("androidx.test:rules:1.4.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
     implementation("com.github.sundeepk:compact-calendar-view:3.0.0")
 }
