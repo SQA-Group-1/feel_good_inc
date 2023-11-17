8f0abbcb891b90f8bde35310c992584d628ca0d9
Isaac Destura <106123286+nimitz-Bear@users.noreply.github.com>
2023-11-17 20:25:59 +0000

    Refactor: use services instead of classes for `Journal` and `Mood` (#72)
    * refactor: use services
    
    * chore: delete uneeded database helpers
    
    * docs: add javadoc and hopefully fix ci error
    
    ---------
    
    Co-authored-by: Isaac Destura <psyid2@exmail.nottingham.ac.uk>

--- app/src/main/java/com/example/feelgoodinc/fragments/MoodFragment.java
+++ app/src/main/java/com/example/feelgoodinc/fragments/MoodFragment.java
@@ -1,12 +1,18 @@
 package com.example.feelgoodinc.fragments;
 
 
-
 import android.annotation.SuppressLint;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.Color;
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
 
+import android.os.IBinder;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +23,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.feelgoodinc.R;
-import com.example.feelgoodinc.database.JournalDatabaseHelper;
-import com.example.feelgoodinc.database.MoodDatabaseHelper;
 import com.example.feelgoodinc.models.Journal;
 import com.example.feelgoodinc.models.Mood;
+import com.example.feelgoodinc.services.JournalService;
+import com.example.feelgoodinc.services.MoodService;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.text.SimpleDateFormat;
@@ -31,19 +37,82 @@ import java.util.Date;
  * A Fragment that allows the user to record their mood and associated journal entry for a given date.
  * The user can choose a mood from a set of predefined options and enter a journal text.
  * The selected mood and journal entry can be confirmed and saved to a database.
- *
+ * <p>
  * This fragment includes UI elements such as a RadioGroup for mood selection, a button to confirm
  * the mood and journal entry.
- *
+ * <p>
  * The class utilizes a MoodDatabaseHelper and JournalDatabaseHelper for managing mood and journal data storage.
  * MoodFragment also provides a method to convert the selected mood text to the corresponding MoodType.
- *
  */
 public class MoodFragment extends Fragment {
-    JournalDatabaseHelper journalDatabaseHelper;
-    MoodDatabaseHelper moodDatabaseHelper;
     Journal journalObj;
     Mood moodObj;
+    MoodService moodService;
+    JournalService journalService;
+    boolean moodBound = false, journalBound = false;
+
+    //region service
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // Unbind from the Mood service
+        getContext().stopService(new Intent(getContext(), MoodService.class));
+        getContext().unbindService(moodConnection);
+
+        // Unbind from the Journal Service
+        getContext().stopService(new Intent(getContext(), JournalService.class));
+        getContext().unbindService(journalConnection);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Bind activity to service
+        Intent intent = new Intent(this.getActivity(), MoodService.class);
+        getActivity().bindService(intent, moodConnection, Context.BIND_AUTO_CREATE);
+
+        //  Bind activity to Journal service
+        Intent moodIntent = new Intent(this.getActivity(), JournalService.class);
+        getActivity().bindService(moodIntent, journalConnection, Context.BIND_AUTO_CREATE);
+    }
+
+
+
+    protected ServiceConnection moodConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d("COMP3013", "onServiceConnected");
+            MoodService.LocalBinder binder = (MoodService.LocalBinder) service;
+            moodService = binder.getService();
+            moodBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.d("COMP3013", "onServiceDisconnected");
+            moodBound = false;
+        }
+    };
+
+    protected ServiceConnection journalConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d("COMP3013", "onServiceConnected");
+            JournalService.LocalBinder binder = (JournalService.LocalBinder) service;
+            journalService = binder.getService();
+            journalBound = true;
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+    //endregion
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -58,16 +127,13 @@ public class MoodFragment extends Fragment {
         String dateStr = currentDate.format(todayDate);
         dateText.setText(dateStr);
 
-        moodDatabaseHelper = new MoodDatabaseHelper();
-        journalDatabaseHelper = new JournalDatabaseHelper();
-
         RadioButton selectedRadioButton = view.findViewById(moodsGroup.getCheckedRadioButtonId());
         selectedRadioButton.setTextColor(Color.BLUE);
 
         confirmButton.setOnClickListener(view1 -> {
             String selectedMood = "";
             int selectedId = moodsGroup.getCheckedRadioButtonId();
-            if(selectedId != -1) {
+            if (selectedId != -1) {
                 RadioButton moodButton = (RadioButton) view.findViewById(selectedId);
                 selectedMood = moodButton.getText().toString();
                 moodObj = new Mood(convertTextToMoodType(selectedMood), todayDate);
@@ -75,30 +141,31 @@ public class MoodFragment extends Fragment {
 
             String journalText = String.valueOf(journalEntry.getText());
             String journalTitle = String.valueOf(journalName.getText());
-            if(journalText.length() == 0 || journalTitle.length() == 0) {
+            if (journalText.length() == 0 || journalTitle.length() == 0) {
                 Toast.makeText(getActivity(), "Don't forget to write something for your feeling!", Toast.LENGTH_SHORT).show();
-            }
-            else{
+            } else {
                 journalObj = new Journal(journalTitle, todayDate, todayDate, journalText);
-                journalDatabaseHelper.addNewJournal(journalObj, getActivity());
-                moodDatabaseHelper.addNewMood(moodObj,getActivity());
+                if (moodBound) {
+                    moodService.addNewMood(moodObj, getActivity());
+                }
+                if (journalBound) {
+                    journalService.addNewJournal(journalObj, getActivity());
+                }
+
             }
         });
 
 
-        moodsGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton selectedRadioButton = view.findViewById(checkedId);
+        moodsGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            RadioButton selectedRadioButton1 = view.findViewById(checkedId);
 
-                for (int i = 0; i < moodsGroup.getChildCount(); i++) {
-                    RadioButton radioButton = (RadioButton) moodsGroup.getChildAt(i);
+            for (int i = 0; i < moodsGroup.getChildCount(); i++) {
+                RadioButton radioButton = (RadioButton) moodsGroup.getChildAt(i);
 
-                    if (radioButton == selectedRadioButton) {
-                        radioButton.setTextColor(Color.BLUE); // Change the text color for the selected radio button
-                    } else {
-                        radioButton.setTextColor(Color.GRAY); // Set the text color for other radio buttons to gray
-                    }
+                if (radioButton == selectedRadioButton1) {
+                    radioButton.setTextColor(Color.BLUE); // Change the text color for the selected radio button
+                } else {
+                    radioButton.setTextColor(Color.GRAY); // Set the text color for other radio buttons to gray
                 }
             }
         });
